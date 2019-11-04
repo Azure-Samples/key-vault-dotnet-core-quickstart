316ab9d1510c2c780ac1c2f2be3a3e3823447046
Matthew Baldwin <mbaldwin@microsoft.com>
2019-11-04 13:20:56 -0800

    Added new sample for track-2 quickstart

---
+++ key-vault-console-app/Program.cs
@@ -0,0 +1,56 @@
+﻿// <directives>
+using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+// </directives>
+
+namespace key_vault_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretName = "mySecret";
+
+            // <authenticate>
+            string keyVaultName = Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
+            var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+            // </authenticate>
+
+            Console.Write("Input the value of your secret > ");
+            string secretValue = Console.ReadLine();
+
+            Console.Write("Creating a secret in " + keyVaultName + " called '" + secretName + "' with the value '" + secretValue + "` ...");
+
+            // <setsecret>
+            client.SetSecret(secretName, secretValue);
+            // </setsecret>
+
+            Console.WriteLine(" done.");
+
+            Console.WriteLine("Forgetting your secret.");
+            secretValue = "";
+            Console.WriteLine("Your secret is '" + secretValue + "'.");
+
+            Console.WriteLine("Retrieving your secret from " + keyVaultName + ".");
+
+            // <getsecret>
+            Secret secret = client.GetSecret(secretName);
+            // </getsecret>
+
+            Console.WriteLine("Your secret is '" + secret.Value + "'.");
+
+            Console.Write("Deleting your secret from " + keyVaultName + " ...");
+
+            // <deletesecret>
+            client.DeleteSecret(secretName);
+            // </deletesecret>
+
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine(" done.");
+
+        }
+    }
+}
